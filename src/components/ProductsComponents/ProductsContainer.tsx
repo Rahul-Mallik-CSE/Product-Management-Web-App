e807eefb9ce60e7997207c4b5d776075d31fff96
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2026-03-19 16:34:14 +0600

    Added @ant-design/nextjs-registry and antd to dependencies in package.json

--- src/components/ProductsComponents/ProductsContainer.tsx
+++ src/components/ProductsComponents/ProductsContainer.tsx
@@ -5,6 +5,7 @@ import { Title } from "../StyledComponents/Title";
 const ProductsContainer = () => {
   return (
     <div className="w-full space-y-4">
+      {/* This is the products page title and it is styled using the Title component from the StyledComponents folder. */}
       <Title>Products List</Title>
     </div>
   );
