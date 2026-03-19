c919b7d6f6cec9c2241aefa05e0068d15cb30b3f
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2026-03-19 16:43:42 +0600

    installed the redux toolkit and create a redux folder then setup the base api only...

---
+++ src/redux/api/baseAPI.tsx
@@ -0,0 +1,15 @@
+/** @format */
+
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const baseApi = createApi({
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({
+    baseUrl: import.meta.env.VITE_API_URL, // Use VITE_API_URL from .env for the base URL
+  }),
+
+  tagTypes: [],
+  endpoints: () => ({}),
+});
+
+export default baseApi;
